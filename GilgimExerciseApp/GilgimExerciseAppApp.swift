3594965e201c6d1f9565fa51eb1f2ac263078730
WooJins <waterather21@gaeasoft.co.kr>
2025-11-05 16:32:02 +0900

    Initial Commit

---
+++ GilgimExerciseApp/GilgimExerciseAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GilgimExerciseAppApp.swift
+//  GilgimExerciseApp
+//
+//  Created by gaea on 11/5/25.
+//
+
+import SwiftUI
+
+@main
+struct GilgimExerciseAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
